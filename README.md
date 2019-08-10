97a5cd8a3cb5beb1f1c771e57ef687b2bdb0f432
Иван Карло <suckawaythepen@gmail.com>
2019-08-10 14:25:06 +0300

    Добавлена проверка существования VPN, обнволение README.md Наряд 0000046003

--- README.md
+++ README.md
@@ -7,7 +7,16 @@
 
 Скрипты PowerShell
 
-* Win VPN.ps1 - Создание, подключение, разъединение и удаление Windows VPN соединения по данным клиента (Пример); 
+* Test-VPN.ps1 - Создание, подключение, разъединение и удаление Windows VPN соединения по данным клиента;
   `.\Test-Vpn.ps1 -NameVpnConnection "Adas Pack" -ServerAddress 213.135.90.163 -TunnelType Pptp -EncryptionLevel Optional -AuthenticationMethod MSChapv2 -Login "4bis" -Password "!346082Bh" -SplitTunneling -PassThru`
-* Win OpenVPN.ps1 Для OpenVPN (В разработке). Для проверки используйте команду 
+* Test-OpenVPN.ps1 - Для OpenVPN. Для проверки используйте команду;
   `.\Test-Openvpn.ps1 -Config "C:\Program Files\OpenVPN\config\private.ovpn" -Ping 10.10.10.0 -TestCmdexe -TestService -TestGui`
+
+Перед использованием скриптов проверьте текущие параметры политики, введя команду `Get-ExecutionPolicy`. Результатом будет одно из следующих значений:
+
+* Restricted — выполнение скриптов запрещено. Стандартная конфигурация;
+* AllSigned — можно запускать скрипты, подписанные доверенным разработчиком; перед запуском скрипта PowerShell запросит у вас подтверждение;
+* RemoteSigned — можно запускать собственные скрипты или те, что подписаны доверенным разработчиком;
+* Unrestricted — можно запускать любые скрипты.
+
+Для начала работы необходимо изменить настройку политики запуска на RemoteSigned, используя команду `Set-ExecutionPolicy`.
--- Scripts/VPN/Test-Vpn.ps1
+++ Scripts/VPN/Test-Vpn.ps1
@@ -1,30 +1,3 @@
-<#  Скрипт предназначен для автоматизации проверки доступов из itilium3
-    При проверке шаги требуется выполнять последовательно #>
-
-<#  Шаг1: Проверка имени пользователя и 
-    Создание VPN подключения #>
-#$WinUserName = "ikarlo";
-#if ($env:UserName -eq $WinUserName) {Add-VpnConnection -Name "Adas Pack Test PowerShell" -ServerAddress "213.135.90.163" -TunnelType Pptp -EncryptionLevel Optional -AuthenticationMethod MSChapv2 -SplitTunneling -PassThru}
-
-<#  Шаг2: Проверка статуса VPN подключения 
-    Соединение VPN подключения #>
-#$vpnName = "Adas Pack Test PowerShell";
-#$login   = "4bis";
-#$pwd     = "!346082Bh";
-#$vpn = Get-VpnConnection -Name $vpnName;
-#if($vpn.ConnectionStatus -eq "Disconnected"){rasdial $vpnName $login $pwd}
-
-<#  Шаг3: Проверка статуса VPN подключения 
-    Отключение VPN подключения #>
-#$vpnName = "Adas Pack Test PowerShell";
-#$vpn = Get-VpnConnection -Name $vpnName;
-#if($vpn.ConnectionStatus -eq "Connected"){rasdial $vpnName /DISCONNECT}
-
-<#  Шаг4: Удаление VPN подключения #>
-#$vpnName = "Adas Pack Test PowerShell";
-#Remove-VpnConnection -Name $vpnName -Force -PassThru
-
-####### Новая итерация скрипта
 
 Param (
     [string]$NameVpnConnection,
@@ -57,6 +30,22 @@ Function Show-Usage {
     Write-Host "Example: .\Test-Vpn.ps1 -NameVpnConnection ""Adas Pack"" -ServerAddress 213.135.90.163 -TunnelType Pptp -EncryptionLevel Optional -AuthenticationMethod MSChapv2 -Login ""4bis"" -Password ""!346082Bh"" -SplitTunneling -PassThru"
 }
 
+Function Check-Availability ([string]$Name) {
+    $FindVpnConnection = Get-VpnConnection | WHERE {$_.Name -eq $Name} | SELECT Name
+    if ($FindVpnConnection) {
+        return 1
+    } else {
+        return 2
+    }
+}
+
+Function Remove-Connection ([string]$Name) {
+    Remove-VpnConnection -Name $Name -Force -PassThru
+}
+
+$res = Check-Availability ($NameVpnConnection);
+if($res -eq 1){Remove-Connection ($NameVpnConnection)}
+
 Add-VpnConnection -Name $NameVpnConnection -ServerAddress $ServerAddress -TunnelType $TunnelType -EncryptionLevel $EncryptionLevel -AuthenticationMethod $AuthenticationMethod -SplitTunneling -PassThru
 
 $vpn = Get-VpnConnection -Name $NameVpnConnection;
@@ -65,5 +54,5 @@ if($vpn.ConnectionStatus -eq "Disconnected"){rasdial $NameVpnConnection $Login $
 $vpn = Get-VpnConnection -Name $NameVpnConnection;
 if($vpn.ConnectionStatus -eq "Connected"){rasdial $NameVpnConnection /DISCONNECT}
 
-Remove-VpnConnection -Name $NameVpnConnection -Force -PassThru
+Remove-Connection ($NameVpnConnection)
 
